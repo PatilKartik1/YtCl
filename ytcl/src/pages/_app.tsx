6e1e81a6f6a24abc49fc180ad5c8079460b97798
Patilkartik1 <132817019+PatilKartik1@users.noreply.github.com>
2026-05-29 21:52:20 +0530

    Completed navbar and added Sidebar

--- ytcl/src/pages/_app.tsx
+++ ytcl/src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="min-h-screen bg-white text-black">
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
