e1a27735b21549f8963ea193fbf3871a007213e9
Patilkartik1 <132817019+PatilKartik1@users.noreply.github.com>
2026-06-01 01:22:50 +0530

    Done with FireBase Integration

--- ytcl/next.config.ts
+++ ytcl/next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
   reactStrictMode: true,
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
 };
 
 export default nextConfig;
