2b72dadaf2407d9e2aa54a31aebc1e2d43768365
Patilkartik1 <132817019+PatilKartik1@users.noreply.github.com>
2026-05-31 22:29:23 +0530

    Started working on backend and connected to mongoDb

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("You tube backend is working");
+});
+
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
+
+const DBURL = process.env.DB_URL;
+
+mongoose
+  .connect(DBURL)
+  .then(() => {
+    console.log("Mongodb connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
