fc77071aa1ccf7c746d2fd1a0b0ddfcd7a602844
Patilkartik1 <132817019+PatilKartik1@users.noreply.github.com>
2026-06-01 01:00:44 +0530

    Added FireBase,js

--- ytcl/src/lib/firebase.js
+++ ytcl/src/lib/firebase.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
-  authDomain: "yourtube-8cda9.firebaseapp.com",
-  projectId: "yourtube-8cda9",
-  storageBucket: "yourtube-8cda9.firebasestorage.app",
-  messagingSenderId: "921641878423",
-  appId: "1:921641878423:web:0d65801eebaf2b25f03ad2",
+  apiKey: "AIzaSyCcFvvw-ix4T9JZ9_Cl2Cqz4DDlLIS6xbU",
+  authDomain: "ytcl-96e90.firebaseapp.com",
+  projectId: "ytcl-96e90",
+  storageBucket: "ytcl-96e90.firebasestorage.app",
+  messagingSenderId: "506403152619",
+  appId: "1:506403152619:web:33f6205464a5e0e61494ae",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-export { auth, provider };
