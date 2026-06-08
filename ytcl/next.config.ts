5632b3fc013fffa2dceddf2a5a01b11a821f922b
Patilkartik1 <132817019+PatilKartik1@users.noreply.github.com>
2026-06-08 22:25:11 +0530

    made changes

--- ytcl/next.config.ts
+++ ytcl/next.config.ts
@@ -7,5 +7,6 @@ const nextConfig: NextConfig = {
     BACKEND_URL: process.env.BACKEND_URL,
   },
 };
+const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/your-route`);
 
 export default nextConfig;
